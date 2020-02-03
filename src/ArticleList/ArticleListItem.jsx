7afc73d95976d2eba3f0cb4570c09ef1fb0072ac
fancytseng <55032412+fancytseng@users.noreply.github.com>
2020-02-02 23:43:13 -0800

    assignment 4

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,21 +1,19 @@
-import React from 'react';
+import React from "react";
 
-class ArticleListItem extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const ArticleListItem = props => {
+    return (         
+        <div>
+            <head>
+                <title>Articles</title>
+            </head>
+            <body>
+                <h2>{props.article.title}</h2>
+                <p>{props.article.shortText}</p>
+                <time dateTime = {props.article.pubYear}>{props.article.pubDate}</time>             
+                <button onClick = {() => alert(props.article.slug)}>show article slug</button>             
+            </body>
 
-  render() {
-    return (
-      <div>
-        <h1>{this.props.article.title}</h1>
-        <p>{this.props.article.shortText}</p>
-        <time dateTime={this.props.article.pubYear}>{this.props.article.pubDate}</time><button onClick={()=>alert(this.props.article.slug)}>show article slug</button>
-      </div>
-    );
-  }
-
+        </div>       
+    )
 }
-
 export default ArticleListItem;
