6bfa3699ebf8f95d464c7436688b1ec78626bda8
fancytseng <55032412+fancytseng@users.noreply.github.com>
2020-02-18 15:45:49 -0800

    Assignment6

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import styles from "./ArticleListItem.module.css";
 import SlugButton from "./SlugButton";
+import ArticleImage from "./ArticleImage";
 
 const ArticleListItem = props => {
   return (
     <div className={styles.container}>
+    <div className={styles.img}>
+      <ArticleImage article={props.article}/>
+    </div>
       <h3 className={styles.title}>{props.article.title}</h3>
       <p>{props.article.shortText}</p>
       <time className={styles.pubDate} dateTime={props.article.pubYear}>{props.article.pubDate}</time>
